9ac96e3fb6cca681719c6fc17fe5f88640af867e
Tomas Hlavacek <tmshlvck@gmail.com>
2022-07-24 16:37:08 +0200

    Refactor the IP match to be also a library
    Add objects for IPTable use cases: VRPS and RTSim (routing table
    simulator).

--- ipm.py
+++ ipm.py
@@ -4,7 +4,7 @@
 """
 ipm - IP match
 
-Copyright (C) 2014-2021 Tomas Hlavacek (tmshlvck@gmail.com)
+Copyright (C) 2014-2022 Tomas Hlavacek (tmshlvck@gmail.com)
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
@@ -17,6 +17,7 @@ You should have received a copy of the GNU General Public License along with
 this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
+from typing import *
 import ipaddress
 import click
 import csv
@@ -82,6 +83,8 @@ class IPTree:
 
 
   def dump(self, point=None):
+    # Warning: The value of internal nodes are set to None. This dumps the raw tree,
+    # but post-processing is needed to obtain only data-carrying tree nodes.
     if point == None:
       point = self.root
 
@@ -99,7 +102,7 @@ class IPTree:
       if point.key == key:
         return point
 
-    raise IndexError(f"{key} not found")
+    raise KeyError(f"{key} not found")
 
 
   def lookupExact(self, key):
@@ -119,7 +122,7 @@ class IPTree:
     if best:
       return best.value
     else:
-      raise IndexError(f"{key} not found")
+      raise KeyError(f"{key} not found")
 
 
   def findAll(self, key):
@@ -148,7 +151,7 @@ class IPTree:
     try:
       self.lookupExact(key)
       return True
-    except IndexError:
+    except KeyError:
       return False
 
 
@@ -180,69 +183,134 @@ def tree_test():
   print(list(pfxs[4].lookupAll(ipaddress.IPv4Address('192.168.1.5'))))
 
 
+class VRPS:
+  def __init__(self):
+    self.trees = { 4: IPTree(4), 6: IPTree(6) }
 
+  @staticmethod
+  def _read_vrps(vrpsfile: IO) -> Generator[None, Tuple[Union[ipaddress.IPv4Network,ipaddress.IPv6Network], int, str, str], None]:
+    """
+    read VRPS CSV file from routinator
+    vrfpsfile: file object
+    output: generator returning tupes (prefix, maxlen, asn, rir)
 
-
-def read_vrps(vrpsfile):
-  """
-  read VRPS CSV file from routinator
-  """
-  rdr = csv.reader(vrpsfile)
-  for r in rdr:
-    try:
-      addr = ipaddress.ip_network(r[1].strip())
-      yield (addr, int(r[2].strip()), r[0].strip(), r[3].strip()) # generate tuples (ipaddress, maxlen, asn, rir)
-    except:
-      pass
-
-
-def read_csv(csvfile):
-  """
-  read arbitrary CSV file and find IP network in each line
-  """
-  rdr = csv.reader(csvfile)
-  for r in rdr:
-    for e in r:
+    example input line: AS1299,103.29.146.0/24,24,apnic
+    example output: (IPv4Network(103.29.146.0/24), 24, 'AS1299', 'apnic')
+    """
+    rdr = csv.reader(vrpsfile)
+    for r in rdr:
       try:
-        addr = ipaddress.ip_network(e.strip())
-        yield (addr, r) # generate tuples (ipaddress, CSV row)
-        break # go to next row in CSV
+        addr = ipaddress.ip_network(r[1].strip())
+        yield (addr, int(r[2].strip()), r[0].strip(), r[3].strip()) # generate tuples (ipaddress, maxlen, asn, rir)
       except:
         pass
 
-
-def read_linux_rt(lrtfile):
-  """
-  read file with ip -n route >$file or ip -n -6 route >$file
-  """
-  for l in lrtfile:
-    try:
-      grps = l.strip().split(' ', 1)
-      addr = ipaddress.ip_network(grps[0])
-      yield (addr, grps[1]) # generate tuples (ipaddress, rest of the row)
-    except:
-      raise
-
-
-
-def normalize_input(instr):
-  DELIMITERS = [' ', ',']
-
-  def normalize_line(l):
-    ls = l.strip()
-    for d in DELIMITERS:
-      spl = ls.split(d, 1)
-      if len(spl) == 2:
-        return spl
-    else:
-      return (ls,None)
-
-  for l in instr:
-    yield normalize_line(l)
+  def readVRPS(self, vrpsfile: IO):
+    """
+    read open VRPS CSV file from routinator
+    use with external open:
+    vrps = VRPS()
+    with open('vrps.csv', 'r') as fh
+      vrps.read_vrps(fh)
+    """
+    for pfx,ml,asn,rir in self._read_vrps(vrpsfile):
+      if not pfx in self.trees[pfx.version]:
+        self.trees[pfx.version][pfx] = set()
+      self.trees[pfx.version][pfx].add((pfx,ml,asn,rir))
 
 
-def output(ip, indata, outdata):
-  print(f"{ip},{str(indata) if indata else ''},{str(outdata)}")
+  def readVRPSFromFile(self, vrpsfilename: str):
+    with open(vrpsfilename, 'r') as fh:
+      return self.readVRPS(fh)
+
+
+  def matchPfx(self, inpfx: Union[ipaddress.IPv4Network,ipaddress.IPv6Network]) -> Generator[None, Tuple[Union[ipaddress.IPv4Network,ipaddress.IPv6Network], int, str, str], None]:
+    """
+    Find all relevant VRPS for a prefix
+    returns Generator of tuples (pfx:Union[ipaddress.IPv4Network,ipaddress.IPv6Network], ml: int, asn: str, rir: str)
+    """
+    for node in self.trees[inpfx.version].traverse(inpfx):
+      if node.value:
+        for nv in node.value:
+          pfx,ml,asn,rir = nv
+          if inpfx.prefixlen <= ml or inpfx.num_addresses == 1:
+              yield (pfx,ml,asn,rir)
+      
+
+
+class RTSim:
+  def __init__(self):
+    self.trees = { 4: IPTree(4), 6: IPTree(6) }
+
+  @staticmethod
+  def _read_csv(csvfile):
+    """
+    read arbitrary CSV file and find IP network in each line
+    """
+    rdr = csv.reader(csvfile)
+    for r in rdr:
+      for e in r:
+        try:
+          addr = ipaddress.ip_network(e.strip())
+          yield (addr, r) # generate tuples (ipaddress, CSV row)
+          break # go to next row in CSV
+        except:
+          pass
+
+  @staticmethod
+  def _read_linux_rt(lrtfile):
+    """
+    read file with ip -N route >$file or ip -N -6 route >$file
+    """
+    def guess_afi(l):
+      for g in l.split(' '):
+        try:
+          dip = ipaddress.ip_network(g)
+          return dip.version
+        except:
+          pass
+      return None
+
+
+    for l in lrtfile:
+      l = l.strip()
+      try:
+        grps = l.split(' ', 1)
+        if grps[0] == 'default':
+          afi = guess_afi(l)
+          if afi == 4:
+            yield (ipaddress.IPv4Network('0.0.0.0/0'), l)
+          elif afi == 6:
+            yield (ipaddress.IPv6Network('::/0'), l)
+          else:
+            raise RuntimeError(f'Can not detect AFI for default route: {l}')
+        else:
+          yield (ipaddress.ip_network(grps[0]), l) # generate tuples (IPv{4,6}Network, RT row)
+      except:
+        raise
+
+  def readRT(self, infile: IO, form="csv"):
+    """
+    rtfile: open file handle to read input from
+    form: "linuxrt"|"csv" - the format of the input
+    """
+    if form == 'linuxrt':
+    # we match IP addresses in a routing table, using the longest prefix match 
+      rtgen = self._read_linux_rt(infile)
+
+    elif form == 'csv':
+    # the same, but we have the "routing table" in a CSV file
+      rtgen = self._read_csv(infile)
+
+    for a,r in rtgen:
+      if not a in self.trees[a.version]:
+        self.trees[a.version][a] = []
+      self.trees[a.version][a].append(r)
+
+
+  def matchIP(self, ipa):
+    return self.trees[ipa.version].lookupLongestPrefix(ipa)
+
 
 
 @click.command(help='find IP subnets that contain IP addresses from args or STDIN\nThe args or STDIN has to contain IP address in the beginning, then more data can be present (delimited by space or comma)')
@@ -252,60 +320,66 @@ def output(ip, indata, outdata):
 @click.option('-n', '--include-no-match', 'nomatchincl', help="include lines with no matche (with empty resut)", is_flag=True)
 @click.argument('ips', nargs=-1)
 def main(csvfile, routetable, vrpsfile, ips, nomatchincl):
-  pfxs = { 4: IPTree(4), 6: IPTree(6) }
+  def normalize_input(instr):
+    DELIMITERS = [' ', ',']
+
+    def normalize_line(l):
+      ls = l.strip()
+      for d in DELIMITERS:
+        spl = ls.split(d, 1)
+        if len(spl) == 2:
+          return spl
+      else:
+        return (ls,None)
+
+    for l in instr:
+      yield normalize_line(l)
+
+  def output(ip, indata, outdata):
+    print(f"{ip},{str(indata) if indata else ''},{str(outdata)}")
 
   if not ips:
     ips = sys.stdin
 
   if routetable or csvfile:
+    rtsim = RTSim()
+
     # we match IP addresses in a routing table, using the longest prefix match 
     if routetable:
-      rtgen = read_linux_rt(csvfile)
+      rtsim.readRT(routetable, 'linuxrt')
 
     # the same, but we have the "routing table" in a CSV file
     if csvfile:
-      rtgen = read_csv(csvfile)
-
-    for a,r in rtgen:
-      if not a in pfxs[a.version]:
-        pfxs[a.version][a] = []
-      pfxs[a.version][a].append(r)
+      rtsim.readRT(csvfile, 'csv')
 
     for ipa,indata in normalize_input(ips):
       ipap = ipaddress.ip_address(ipa)
       try:
-        res = pfxs[ipap.version].lookupLongestPrefix(ipap)
+        res = rtsim.matchIP(ipap)
         if res:
           for r in res:
             output(ipa, indata, r)
         else:
           if nomatchincl:
             output(ipa, indata, None)
-      except IndexError:
+      except KeyError:
         if nomatchincl:
           output(ipa, indata, None)
 
   # this is specical case: we match IP networks in a VRPS table, which is an IP network filter
   # and it contains networks and maxlens
   elif vrpsfile:
-    for pfx,ml,asn,rir in read_vrps(vrpsfile):
-      if not pfx in pfxs[pfx.version]:
-        pfxs[pfx.version][pfx] = set()
-      pfxs[pfx.version][pfx].add((str(pfx),ml,asn,rir))
-
+    vrps = VRPS()
+    vrps.readVRPS(vrpsfile)
+    
     for ipn,indata in normalize_input(ips):
       ipnp = ipaddress.ip_network(ipn.strip())
 
-      matches = set()
-      for node in pfxs[ipnp.version].traverse(ipnp):
-        if node.value:
-          for nv in node.value:
-            pfx,ml,asn,rir = nv
-            if ipnp.prefixlen <= ml or ipnp.num_addresses == 1:
-              matches.add(nv)
-      output(ipn, indata, matches)
+      matches = set(vrps.matchPfx(ipnp))
+      output(ipn, indata, [(str(m[0]),)+tuple(m[1:]) for m in matches])
 
 
 if __name__ == '__main__':
 #  tree_test()
   main()
+
